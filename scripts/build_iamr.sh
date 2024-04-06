7e0f435e348985933c9b70bfbc2b91859453fd0f
Eduardo Kussler <eduardoeugeniok@gmail.com>
2024-04-06 11:15:01 -0300

    up: loop through IAMR tutorial folder and build each example

--- scripts/build_iamr.sh
+++ scripts/build_iamr.sh
@@ -6,3 +6,21 @@ AMREX_HYDRO_HOME=../projetos/dependencies/AMReX-Hydro/
 USE_CUDA=TRUE # -> Set USE_OMP to FALSE
 COMP=gnu
 # Then, just use make
+
+TUTORIALS_PATH=../projetos/IAMR/Tutorials
+
+cd $TUTORIALS_PATH
+pwd
+
+shopt -s nullglob
+shopt -s dotglob
+
+EXAMPLES=(./*)
+
+
+for test in ${EXAMPLES[@]}; do
+    echo $test
+    cd $test
+    make -j 8
+    cd ..
+done
