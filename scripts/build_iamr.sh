bc43d6e139f0912ff948c1cc0d8bf7b2b94c0d87
Eduardo Kussler <eduardoeugeniok@gmail.com>
2024-04-06 10:39:08 -0300

    up: add steps to build IAMR examples

---
+++ scripts/build_iamr.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+# Set these on the GNUMakefile for each tutorial example you want to use
+AMREX_HOME=../projetos/dependencies/amrex/
+AMREX_HYDRO_HOME=../projetos/dependencies/AMReX-Hydro/
+USE_CUDA=TRUE # -> Set USE_OMP to FALSE
+COMP=gnu
+# Then, just use make
