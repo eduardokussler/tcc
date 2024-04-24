9ccb43929004e1fe9eaa83c47c61b423bd7077a5
Eduardo Kussler <eduardoeugeniok@gmail.com>
2024-04-23 22:05:17 -0300

    up: add run_hypar to orchestrator script; Add "new platform", that is my laptop, to skip tests that do not run on it

--- scripts/orchestrator/run_orchestrator.sh
+++ scripts/orchestrator/run_orchestrator.sh
@@ -1,11 +1,14 @@
 PREFIX="../run/"
-SCRIPTS=(run_examiniMD.sh run_quicksilver.sh run_goulash)
-INTERVALS=(0.1              1                1)
-
-PLATFORM=$1
+SCRIPTS=(run_examiniMD.sh   run_quicksilver.sh run_goulash run_hypar)
+INTERVALS=(0.1              1                  1           1)
+PLATFORM_PARAM=$1
+PLATFORM=$PLATFORM_PARAM
+if [ $PLATFORM = "eekussler_laptop" ]; then
+    PLATFORM="geforce"
+fi
 
 if [ -z $PLATFORM ]; then
-    echo "The platform is required! Options are [geforce, gppd, enterprise]";
+    echo "The platform is required! Options are [geforce, gppd, enterprise, eekussler_laptop]";
     exit 1
 fi
 
@@ -14,6 +17,12 @@ INDEX=0
 for SCRIPT in ${SCRIPTS[@]}
 do
     #echo ${INTERVALS[$INDEX]}
+    # This test uses too much memory and doesn't run on my laptop
+    if [ $PLATFORM_PARAM = "eekussler_laptop" ] && [ $SCRIPT = "run_hypar" ]; then
+        echo "$SCRIPT is not supported in this platform ($PLATFORM_PARAM)! The "
+        INDEX=$((INDEX+1))
+        continue
+    fi
     sudo python3 main.py -r $PREFIX$SCRIPT -p $PLATFORM -i ${INTERVALS[$INDEX]}
     INDEX=$((INDEX+1))
 done
